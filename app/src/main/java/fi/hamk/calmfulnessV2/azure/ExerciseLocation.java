d4de85f03abd7caaeef2159a44d4a81f257c4810
Toni Lavonen <toni.lavonen@student.hamk.fi>
2018-03-26 14:12:30 +0300

    Updated dependence version numbers in build.gradle Created new database class exerciseLocation

---
+++ app/src/main/java/fi/hamk/calmfulnessV2/azure/ExerciseLocation.java
@@ -0,0 +1,162 @@
+package fi.hamk.calmfulnessV2.azure;
+
+
+public class ExerciseLocation {
+
+    /**
+     * Unique identifier of a exercise location
+     */
+    @com.google.gson.annotations.SerializedName("id")
+    private String id;
+
+    /**
+     * Returns the ID of the exercise location
+     *
+     * @return ID of the exercise location
+     */
+    public String getExcerciseId() {
+        return id;
+    }
+
+    /**
+     * Sets the unique identifier of the exercise location.
+     *
+     * @param id Unique identifier of the exercise location.
+     */
+    private void setExerciseId(final String id) {
+        this.id = id;
+    }
+
+    /**
+     * Unique identifier of a exercise title in finnish
+     */
+    @com.google.gson.annotations.SerializedName("title_fi")
+    private String titleFI;
+
+    /**
+     * Returns the ID of the title of the exercise in finnish
+     * @return title of the exercise in finnish
+     */
+    public String getTitleFI() {
+        return titleFI;
+    }
+
+    /**
+     * Sets the unique identifier of the finnish exercise title.
+     * @param titleFI title of the exercise in finnish
+     */
+    private void setTitleFI(final String titleFI) {
+        this.titleFI = titleFI;
+    }
+
+    /**
+     * Unique identifier of a exercise title in english
+     */
+    @com.google.gson.annotations.SerializedName("title_en")
+    private String titleEN;
+
+    /**
+     * Returns the ID of the english exercise title
+     * @return title of the exercise in english
+     */
+    public String getTitleEN() {
+        return titleEN;
+    }
+
+    /**
+     * Sets the unique identifier of the english exercise title.
+     * @param titleEN title of the exercise in english
+     */
+    private void setTitleEN(final String titleEN) {
+        this.titleEN = titleEN;
+    }
+
+    /**
+     * Unique identifier of a exercise text in finnish
+     */
+    @com.google.gson.annotations.SerializedName("text_fi")
+    private String textFI;
+
+    /**
+     * Returns the ID of the finnish exercise text
+     * @return textual content of the exercise in finnish
+     */
+    public String getTextFI() {
+        return textFI;
+    }
+
+    /**
+     * Sets the unique identifier of the finnish exercise text
+     * @param textFI textual content of the exercise in finnish
+     */
+    private void setTextFI(final String textFI) {
+        this.textFI = textFI;
+    }
+
+    /**
+     * Unique identifier of a exercise text in english
+     */
+    @com.google.gson.annotations.SerializedName("text_en")
+    private String textEN;
+
+    /**
+     * Returns the ID of the english exercise text
+     * @return textual content of the exercise in english
+     */
+    public String getTextEN() {
+        return textEN;
+    }
+
+    /**
+     * Sets the unique identifier of the english exercise text
+     * @param textEN textual content of the exercise in english
+     */
+    private void setTextEN(final String textEN) {
+        this.textEN = textEN;
+    }
+
+    /**
+     * Unique identifier of a picture
+     */
+    @com.google.gson.annotations.SerializedName("picture")
+    private String picture;
+
+    /**
+     * Returns the ID of the picture
+     * @return URL of the picture
+     */
+    public String getPicture() {
+        return picture;
+    }
+
+    /**
+     * Sets the unique identifier of the picture
+     * @param picture URL of the picture
+     */
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+    /**
+     * Unique identifier of a video
+     */
+    @com.google.gson.annotations.SerializedName("video")
+    private String video;
+
+    /**
+     * Returns the ID of the video
+     * @return URL of the video
+     */
+    public String getVideo() {
+        return video;
+    }
+
+    /**
+     * Sets the unique identifier of the video
+     * @param video URL of the video
+     */
+    public void setVideo(String video) {
+        this.video = video;
+    }
+
+}
