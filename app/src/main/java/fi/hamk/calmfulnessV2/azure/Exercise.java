f0a593c7d85323bb3a24c312527bf5e6bc355f5f
Toni Lavonen <toni.lavonen@student.hamk.fi>
2018-03-26 14:53:30 +0300

    Modified content of the Exercise class

--- app/src/main/java/fi/hamk/calmfulnessV2/azure/Exercise.java
+++ app/src/main/java/fi/hamk/calmfulnessV2/azure/Exercise.java
@@ -4,95 +4,185 @@ package fi.hamk.calmfulnessV2.azure;
  * Class for Exercise table in Azure
  */
 public class Exercise {
+
     /**
-     * Unique identifier of a exercise
+     * Unique identifier of a exercise location
      */
     @com.google.gson.annotations.SerializedName("id")
     private String id;
 
     /**
-     * Returns the ID of the exercise
+     * Returns the ID of the exercise location
      *
-     * @return ID of the exercise
+     * @return ID of the exercise location
      */
     public String getExcerciseId() {
         return id;
     }
 
     /**
-     * Sets the unique identifier of the exercise.
+     * Sets the unique identifier of the exercise location.
      *
-     * @param id Unique identifier of the exercise.
+     * @param id Unique identifier of the exercise location.
      */
     private void setExerciseId(final String id) {
         this.id = id;
     }
 
     /**
-     * Unique identifier of a exercise
+     * Unique identifier of a exercise title in finnish
      */
-    @com.google.gson.annotations.SerializedName("exerciseTitle")
-    private String exerciseTitle;
+    @com.google.gson.annotations.SerializedName("title_fi")
+    private String titleFI;
 
     /**
-     * Returns the title the exercise
-     *
-     * @return Title of the exercise
+     * Returns the ID of the title of the exercise in finnish
+     * @return title of the exercise in finnish
      */
-    public String getExerciseTitle() {
-        return exerciseTitle;
+    public String getTitleFI() {
+        return titleFI;
     }
 
     /**
-     * Sets the title of the exercise.
-     *
-     * @param exerciseTitle Title of the exercise.
+     * Sets the unique identifier of the finnish exercise title.
+     * @param titleFI title of the exercise in finnish
      */
-    private void setExerciseTitle(final String exerciseTitle) {
-        this.exerciseTitle = exerciseTitle;
+    private void setTitleFI(final String titleFI) {
+        this.titleFI = titleFI;
     }
 
     /**
-     * Content of the exercise
+     * Unique identifier of a exercise title in english
      */
-    @com.google.gson.annotations.SerializedName("exerciseContent")
-    private String exerciseContent;
+    @com.google.gson.annotations.SerializedName("title_en")
+    private String titleEN;
 
     /**
-     * Returns the content of an exercise
-     *
-     * @return Content of an exercise
+     * Returns the ID of the english exercise title
+     * @return title of the exercise in english
      */
-    public String getExerciseContent() {
-        return exerciseContent;
+    public String getTitleEN() {
+        return titleEN;
     }
 
     /**
-     * Sets the content of an exercise
-     *
-     * @param exerciseContent Content of an exercise
+     * Sets the unique identifier of the english exercise title.
+     * @param titleEN title of the exercise in english
      */
-    private void setExerciseContent(final String exerciseContent) {
-        this.exerciseContent = exerciseContent;
+    private void setTitleEN(final String titleEN) {
+        this.titleEN = titleEN;
     }
 
     /**
-     * Class for Exercise table in Azure
-     * Empty constructor
+     * Unique identifier of a exercise text in finnish
      */
-    Exercise() {
+    @com.google.gson.annotations.SerializedName("text_fi")
+    private String textFI;
+
+    /**
+     * Returns the ID of the finnish exercise text
+     * @return textual content of the exercise in finnish
+     */
+    public String getTextFI() {
+        return textFI;
     }
 
     /**
-     * Class for Exercise table in Azure
-     *
-     * @param id              ID of a new Exercise.
-     * @param exerciseContent Content of a new Exercise
-     * @param exerciseTitle   Title of a new Exercise
+     * Sets the unique identifier of the finnish exercise text
+     * @param textFI textual content of the exercise in finnish
      */
-    Exercise(final String id, final String exerciseTitle, final String exerciseContent) {
-        this.setExerciseId(id);
-        this.setExerciseContent(exerciseContent);
-        this.setExerciseTitle(exerciseTitle);
+    private void setTextFI(final String textFI) {
+        this.textFI = textFI;
+    }
+
+    /**
+     * Unique identifier of a exercise text in english
+     */
+    @com.google.gson.annotations.SerializedName("text_en")
+    private String textEN;
+
+    /**
+     * Returns the ID of the english exercise text
+     * @return textual content of the exercise in english
+     */
+    public String getTextEN() {
+        return textEN;
+    }
+
+    /**
+     * Sets the unique identifier of the english exercise text
+     * @param textEN textual content of the exercise in english
+     */
+    private void setTextEN(final String textEN) {
+        this.textEN = textEN;
+    }
+
+    /**
+     * Unique identifier of a picture
+     */
+    @com.google.gson.annotations.SerializedName("picture")
+    private String picture;
+
+    /**
+     * Returns the ID of the picture
+     * @return URL of the picture
+     */
+    public String getPicture() {
+        return picture;
+    }
+
+    /**
+     * Sets the unique identifier of the picture
+     * @param picture URL of the picture
+     */
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+    /**
+     * Unique identifier of a video
+     */
+    @com.google.gson.annotations.SerializedName("video")
+    private String video;
+
+    /**
+     * Returns the ID of the video
+     * @return URL of the video
+     */
+    public String getVideo() {
+        return video;
+    }
+
+    /**
+     * Sets the unique identifier of the video
+     * @param video URL of the video
+     */
+    public void setVideo(String video) {
+        this.video = video;
+    }
+
+    /**
+     * Default constructor of a exercise
+     */
+    Exercise() {}
+
+    /**
+     * Constructor of Exercise
+     * @param id Unique identifier of a exercise
+     * @param titleFI Title of the exercise
+     * @param titleEN Title of the exercise
+     * @param textFI Textual content of the exercise in finnish
+     * @param textEN Textual content of the exercise in english
+     * @param picture URL of a picture
+     * @param video URL of a video
+     */
+    public Exercise(String id, String titleFI, String titleEN, String textFI, String textEN, String picture, String video) {
+        this.id = id;
+        this.titleFI = titleFI;
+        this.titleEN = titleEN;
+        this.textFI = textFI;
+        this.textEN = textEN;
+        this.picture = picture;
+        this.video = video;
     }
 }
--- app/src/main/java/fi/hamk/calmfulnessV2/azure/ExerciseLocation.java
+++ app/src/main/java/fi/hamk/calmfulnessV2/azure/ExerciseLocation.java
@@ -1,6 +1,8 @@
 package fi.hamk.calmfulnessV2.azure;
 
-
+/**
+ * Class for Exercise location table in Azure
+ */
 public class ExerciseLocation {
 
     /**
@@ -10,153 +12,80 @@ public class ExerciseLocation {
     private String id;
 
     /**
-     * Returns the ID of the exercise location
-     *
-     * @return ID of the exercise location
+     * Returns identifier of the exercise location
+     * @return Identifier of the exercise location
      */
-    public String getExcerciseId() {
+    public String getId() {
         return id;
     }
 
     /**
-     * Sets the unique identifier of the exercise location.
-     *
-     * @param id Unique identifier of the exercise location.
+     * Sets identifier of the exercise location
+     * @param id Identifier of the exercise location
      */
-    private void setExerciseId(final String id) {
+    public void setId(String id) {
         this.id = id;
     }
 
     /**
-     * Unique identifier of a exercise title in finnish
+     * Unique identifier of a exercise
      */
-    @com.google.gson.annotations.SerializedName("title_fi")
-    private String titleFI;
+    @com.google.gson.annotations.SerializedName("exercise")
+    private String exercise;
 
     /**
-     * Returns the ID of the title of the exercise in finnish
-     * @return title of the exercise in finnish
+     * Returns identifier of the exercise
+     * @return Identifier of the exercise
      */
-    public String getTitleFI() {
-        return titleFI;
+    public String getExercise() {
+        return exercise;
     }
 
     /**
-     * Sets the unique identifier of the finnish exercise title.
-     * @param titleFI title of the exercise in finnish
+     * Sets identifier of the exercise
+     * @param exercise Identifier of the exercise
      */
-    private void setTitleFI(final String titleFI) {
-        this.titleFI = titleFI;
+    public void setExercise(String exercise) {
+        this.exercise = exercise;
     }
 
     /**
-     * Unique identifier of a exercise title in english
+     * Uniquer identifier of a location
      */
-    @com.google.gson.annotations.SerializedName("title_en")
-    private String titleEN;
+    @com.google.gson.annotations.SerializedName("location")
+    private String location;
 
     /**
-     * Returns the ID of the english exercise title
-     * @return title of the exercise in english
+     * Returns identifier of the location
+     * @return Identifier of the location
      */
-    public String getTitleEN() {
-        return titleEN;
+    public String getLocation() {
+        return location;
     }
 
     /**
-     * Sets the unique identifier of the english exercise title.
-     * @param titleEN title of the exercise in english
+     * Sets identifier of the location
+     * @param location Identifier of the location
      */
-    private void setTitleEN(final String titleEN) {
-        this.titleEN = titleEN;
+    public void setLocation(String location) {
+        this.location = location;
     }
 
     /**
-     * Unique identifier of a exercise text in finnish
+     * Default constructor of a ExerciseLocation
      */
-    @com.google.gson.annotations.SerializedName("text_fi")
-    private String textFI;
-
-    /**
-     * Returns the ID of the finnish exercise text
-     * @return textual content of the exercise in finnish
-     */
-    public String getTextFI() {
-        return textFI;
+    public ExerciseLocation() {
     }
 
     /**
-     * Sets the unique identifier of the finnish exercise text
-     * @param textFI textual content of the exercise in finnish
+     * Constructor of a ExerciseLocation
+     * @param id Id of a exercise location
+     * @param exercise Id of a exercise
+     * @param location Id of a location
      */
-    private void setTextFI(final String textFI) {
-        this.textFI = textFI;
+    public ExerciseLocation(String id, String exercise, String location) {
+        this.id = id;
+        this.exercise = exercise;
+        this.location = location;
     }
-
-    /**
-     * Unique identifier of a exercise text in english
-     */
-    @com.google.gson.annotations.SerializedName("text_en")
-    private String textEN;
-
-    /**
-     * Returns the ID of the english exercise text
-     * @return textual content of the exercise in english
-     */
-    public String getTextEN() {
-        return textEN;
-    }
-
-    /**
-     * Sets the unique identifier of the english exercise text
-     * @param textEN textual content of the exercise in english
-     */
-    private void setTextEN(final String textEN) {
-        this.textEN = textEN;
-    }
-
-    /**
-     * Unique identifier of a picture
-     */
-    @com.google.gson.annotations.SerializedName("picture")
-    private String picture;
-
-    /**
-     * Returns the ID of the picture
-     * @return URL of the picture
-     */
-    public String getPicture() {
-        return picture;
-    }
-
-    /**
-     * Sets the unique identifier of the picture
-     * @param picture URL of the picture
-     */
-    public void setPicture(String picture) {
-        this.picture = picture;
-    }
-
-    /**
-     * Unique identifier of a video
-     */
-    @com.google.gson.annotations.SerializedName("video")
-    private String video;
-
-    /**
-     * Returns the ID of the video
-     * @return URL of the video
-     */
-    public String getVideo() {
-        return video;
-    }
-
-    /**
-     * Sets the unique identifier of the video
-     * @param video URL of the video
-     */
-    public void setVideo(String video) {
-        this.video = video;
-    }
-
 }
